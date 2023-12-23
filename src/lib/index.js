bcea750048139ad2fc4325d1d7d81fddf31e8849
HassanHeydariNasab <hsn6@tuta.io>
2023-12-23 20:19:45 +0330

    Get it ready for npm

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import { useAfterKeyPagination } from "./hooks/useAfterKeyPagination";
+import { useSlicePagination } from "./hooks/useSlicePagination";
+import { useHyperPagination } from "./hooks/useHyperPagination";
+
+export { useAfterKeyPagination, useSlicePagination, useHyperPagination };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.js"),
+      name: "react-hyper-pagination",
+      fileName: (format) => `react-hyper-pagination.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   plugins: [react()],
   test: {
     environment: "jsdom",
